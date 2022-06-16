609112d569015336d1b839a7324ed1a4b1022e84
HeiHeiToooo <97014687+HeiHeiToooo@users.noreply.github.com>
2022-06-16 19:15:04 +0800

    Add files via upload

--- Detection Head/DETR/utils.py
+++ Detection Head/DETR/utils.py
@@ -1,42 +1,63 @@
 import torch
 
 
-def box_cxcywh_to_xyxy(x):
-    x_c, y_c, w, h = x.unbind(-1)
-    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
-         (x_c + 0.5 * w), (y_c + 0.5 * h)]
-    return torch.stack(b, dim=-1)
+# combine tensor and mask together
+class NestedTensor(object):
+
+    def __init__(self, tensors, mask):
+        self.tensors = tensors
+        self.mask = mask
+
+    def to(self, device):
+        # casts both tensors and mask to device(CUDA)
+        cast_tensor = self.tensors.to(device)
+        mask = self.mask
+        if mask is not None:
+            assert mask is not None
+            cast_mask = mask.to(device)
+        else:
+            cast_mask = None
+        return NestedTensor(cast_tensor, cast_mask)
+
+    def decompose(self):
+        # returns tensors and mask as a tuple
+        return self.tensors, self.mask
+
+    def __repr__(self):
+        return str(self.tensors)
+# tensor = torch.randn(2,3,765,911)
+# mask = torch.randn(2,765,911)
+# nested = NestedTensor(tensor,mask)
+# nested.tensors.shape --> torch.Size([2,3,765,911])
+# nested.mask.shape --> torch.Size([2,765,911])
 
 
-def generalized_box_iou(box1, box2):
-    # get the value of box1
-    box1_x1 = box1[..., 0:1]  # (N, 4) -> (N, 1)
-    box1_y1 = box1[..., 1:2]
-    box1_x2 = box1[..., 2:3]
-    box1_y2 = box1[..., 3:4]
-
-    # get the value of box2
-    box2_x1 = box2[..., 0:1]
-    box2_y1 = box2[..., 1:2]
-    box2_x2 = box2[..., 2:3]
-    box2_y2 = box2[..., 3:4]
-
-    x1 = torch.max(box1_x1, box2_x1)
-    y1 = torch.max(box1_y1, box2_y1)
-    x2 = torch.min(box1_x2, box2_x2)
-    y2 = torch.min(box1_y2, box2_y2)
-
-    # clamp(0) for the case that no intersection
-    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
-
-    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
-    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
-
-    iou = intersection / (box1_area + box2_area - intersection + 1e-6)
-    return iou
+def _max_by_axis(the_list):
+    # Input: [[3, 224, 230], [3, 230, 224]]
+    # Output: [3,230,230]
+    maxes = the_list[0]
+    for sublist in the_list[1:]:
+        for index, item in enumerate(sublist):
+            maxes[index] = max(maxes[index], item)
+    return maxes
 
 
-if __name__ == '__main__':
-    test_box1 = torch.randn(32, 4).sigmoid()
-    test_box2 = torch.randn(32, 4).sigmoid()
-    print(generalized_box_iou(test_box1, test_box2))
+def nested_tensor_from_tensor_list(tensor_list):
+    if tensor_list[0].ndim == 3:
+        max_size = _max_by_axis([list(img.shape) for img in tensor_list])
+        batch_shape = [len(tensor_list)] + max_size
+        b, c, h, w = batch_shape
+        dtype = tensor_list[0].dtype
+        device = tensor_list[0].device
+        tensor = torch.zeros(batch_shape, dtype=dtype, device=device)
+        mask = torch.ones((b, h, w), dtype=torch.bool, device=device)
+        for img, pad_img, m in zip(tensor_list, tensor, mask):
+            # img 是tensor_list里面的原始图片;torch.Size([3, 224, 230])
+            # pad_img 是填补零以后的图片/tensor，初始全为0；torch.Size([3, 230, 230])
+            # m是mask的tensor，没有Chanel维度，初始全为1；torch.Size([230, 230]) mask值=0 设置为True
+            pad_img[: img.shape[0], : img.shape[1], : img.shape[2]].copy_(img)
+            m[: img.shape[1], :img.shape[2]] = False
+    else:
+        raise ValueError('not supported')
+    return NestedTensor(tensor, mask)
+
