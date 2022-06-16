757638bda9dfb199baf622061f84169ba2c136d4
WU Junyan <85872793+SupercarryNg@users.noreply.github.com>
2022-06-16 13:17:46 +0800

    Add files via upload

---
+++ Detection Head/DETR/utils.py
@@ -0,0 +1,40 @@
+import torch
+
+
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=-1)
+
+
+def generalized_box_iou(box1, box2):
+    # get the value of box1
+    box1_x1 = box1[..., 0:1]  # (N, 4) -> (N, 1)
+    box1_y1 = box1[..., 1:2]
+    box1_x2 = box1[..., 2:3]
+    box1_y2 = box1[..., 3:4]
+
+    # get the value of box2
+    box2_x1 = box2[..., 0:1]
+    box2_y1 = box2[..., 1:2]
+    box2_x2 = box2[..., 2:3]
+    box2_y2 = box2[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    # clamp(0) for the case that no intersection
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    iou = intersection / (box1_area + box2_area - intersection + 1e-6)
+    return iou
+
+
+if __name__ == '__main__':
+    test =
